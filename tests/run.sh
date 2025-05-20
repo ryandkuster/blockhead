443def0210265129231da5406c412f294beb2071
ryandkuster <ryan.kuster@gmail.com>
2025-05-19 20:36:20 -0400

    small updates

--- tests/run.sh
+++ tests/run.sh
@@ -5,3 +5,11 @@ blockhead \
     -o output/sanity_mendelian_biallelic_mier.vcf \
     -x .8 \
     -t 12
+
+blockhead \
+    -v data/mier_test_1.vcf \
+    -p data/mier_test_1_parentage.tsv \
+    -r output/mier_test_1_summary.tsv \
+    -o output/mier_test_1_mier.vcf \
+    -x .8 \
+    -t 12
